8ffc198a368ae9eebf027d021e2e0ec5fba33aac
muhammadfaizan <faizan@daraz.pk>
2018-05-17 12:38:04 +0500

    functional programming example

---
+++ functional-programming.js
@@ -0,0 +1,12 @@
+let heightRequirement = 46;
+
+// Impure because it relies on a mutable (reassignable) variable.
+function canRide(height) {
+  return height >= heightRequirement;
+}
+
+// Impure because it causes a side-effect by logging to the console.
+function multiply(a, b) {
+  console.log('Arguments: ', a, b);
+  return a * b;
+}
