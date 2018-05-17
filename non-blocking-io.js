26497ac8ed416620dc249bfc04200dfb91360d80
muhammadfaizan <faizan@daraz.pk>
2018-05-17 15:03:38 +0500

    non blocking improvement

--- non-blocking-io.js
+++ non-blocking-io.js
@@ -2,11 +2,12 @@
 const callToDatabase = (k, cb) => setTimeout(() => cb(k), Math.random() * 1000);
 
 const print = input => {
-    console.log(input);
+    console.timeEnd('process '+ input);
 }
 for (var i = 0; i < 10; i++) {
     (k => {
         // some pseudo delay
+        console.time('process '+ k)
         callToDatabase(k, print);
     })(i);
 }
